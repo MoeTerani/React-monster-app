863609f433a18c69d2581a257d300d1d3e527108
Moe Terani <45799313+MoeTerani@users.noreply.github.com>
2019-08-23 11:08:24 +0200

    base

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      string: 'Hello Moe!'
+    };
+  }
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: 'Hello Neda!!!!' })}>
+            Change Text
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//***************************
+
+// function App() {
+//   return (
+
+//   );
+// }
+//*************************** */
+
+// class  extends Component {
+//   state = {  }
+//   render() {
+//     return (  );
+//   }
+// }
+
+// export default ;
+
+//******************************** */
+
+// class  extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {  }
+//   }
+//   render() {
+//     return (  );
+//   }
+// }
+
+// export default ;
+
+//***************************
